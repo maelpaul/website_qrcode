6c109eed9cfc0c562f50cc82ccbb90b26e5a57ec
nthivin <nathan.thivin@gmail.com>
2023-12-05 23:03:55 +0100

    manage gps packets

--- server/parser.py
+++ server/parser.py
@@ -1,6 +1,6 @@
 def is_ip_already_in_file(new_ip, filename):
-    with open('../data/'+filename, 'r') as fichier:
-        for ip in fichier:
+    with open('../data/'+filename, 'r') as file:
+        for ip in file:
             if new_ip+"\n" == ip:
                 return True
 
@@ -13,8 +13,8 @@ def receive_paquet(addr, data):
     ip_addr = addr[0]
     
     if not is_ip_already_in_file(ip_addr, "ip.txt"):
-        with open('../data/ip.txt', 'a') as fichier:
-            fichier.write(ip_addr+"\n")
+        with open('../data/ip.txt', 'a') as file:
+            file.write(ip_addr+"\n")
 
     process_packet_type(ip_addr, new_data)
 
@@ -35,19 +35,34 @@ def process_packet_type(ip_addr, new_data):
 
 
 
-def process_gps(ip_addr, new_data):
-    if not is_ip_already_in_file(ip_addr, "gps.txt"): 
-        with open('../data/gps.txt', 'a') as fichier:
-            fichier.write("'" + ip_addr+"','" + new_data + "'\n")
-    # a changer
 
+
+
+def process_gps(ip_addr, new_data):
+    already_here = False
+    with open('../data/gps.txt', 'r') as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            ips = lines[i].split(",")[0]
+            if ips == "'"+ip_addr+"'":
+                lines[i] = "'" + ip_addr+"','" + new_data + "'\n"
+                already_here = True
+                       
+    if already_here : 
+        with open('../data/gps.txt', 'w') as file:
+            file.writelines(lines)
+    else :
+        with open('../data/gps.txt', 'a') as file:
+            file.write("'" + ip_addr+"','" + new_data + "'\n")
+
+           
 
 
 def process_url(ip_addr, new_data):
-    with open('../data/url.txt', 'a') as fichier:
-        fichier.write("'" + ip_addr+"','" + new_data + "'\n")
+    with open('../data/url.txt', 'a') as file:
+        file.write("'" + ip_addr+"','" + new_data + "'\n")
 
 
 def process_text(ip_addr, new_data):
-    with open('../data/text.txt', 'a') as fichier:
-        fichier.write("'" + ip_addr+"','" + new_data + "'\n")
+    with open('../data/text.txt', 'a') as file:
+        file.write("'" + ip_addr+"','" + new_data + "'\n")
