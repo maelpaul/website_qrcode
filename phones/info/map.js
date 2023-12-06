98e634eab6e769d757d9eaa116c438c7a5aa38b2
nthivin <nathan.thivin@gmail.com>
2023-12-06 16:26:18 +0100

    ajout map

---
+++ phones/info/map.js
@@ -0,0 +1,29 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
